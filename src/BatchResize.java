4e8359bf5ee6d3806bf7aeca1156f53114baaefb
Eric Dalquist <eric.dalquist@gmail.com>
2012-06-25 12:56:37 -0500

    Included required libraries, add retry logic to upload & download

--- src/BatchResize.java
+++ src/BatchResize.java
@@ -1,7 +1,6 @@
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Collection;
@@ -12,7 +11,6 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -31,7 +29,6 @@ import com.google.gdata.data.photos.AlbumEntry;
 import com.google.gdata.data.photos.AlbumFeed;
 import com.google.gdata.data.photos.PhotoEntry;
 import com.google.gdata.data.photos.UserFeed;
-import com.google.gdata.util.ServiceException;
 
 /**
  * An effective but gross tool that will resize all of the images in a picasa account
@@ -137,7 +134,7 @@ public class BatchResize {
      * @param wait If true {@link Future#get()} is called on each {@link Future}.
      *             If false it is only called on futures where {@link Future#isDone()} returns true, also the method returns after the first future that returns true is uploaded.  
      */
-    private static void checkResizeFutures(Collection<Future<ResizeRequest>> futures, boolean wait) throws InterruptedException, ExecutionException, IOException, ServiceException {
+    private static void checkResizeFutures(Collection<Future<ResizeRequest>> futures, boolean wait) throws Exception {
         final List<Future<?>> newFutures = new LinkedList<Future<?>>();
         newFutures.clear();
         
@@ -156,39 +153,65 @@ public class BatchResize {
     /**
      * Download the specified image and then submit it to the resize thread pool, returning the resize future.
      */
-    private static Future<ResizeRequest> download(PicasawebService myService, ResizeRequest resizeRequest, ExecutorService resizePool) throws IOException, ServiceException {
-        System.out.println("\t\tDownloading " + resizeRequest.photoEntry.getTitle().getPlainText());
-        final com.google.gdata.data.MediaContent mc = new com.google.gdata.data.MediaContent();
-        mc.setUri(resizeRequest.photoUrl);
-        final MediaSource media = myService.getMedia(mc);
-        final InputStream input = media.getInputStream();
-        try {
-            final BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(resizeRequest.originalFile));
+    private static Future<ResizeRequest> download(PicasawebService myService, ResizeRequest resizeRequest, ExecutorService resizePool) throws Exception {
+        for (int t = 0; ; t++) {
             try {
-                IOUtils.copy(input, output);
+                System.out.println("\t\tDownloading " + resizeRequest.photoEntry.getTitle().getPlainText());
+                final com.google.gdata.data.MediaContent mc = new com.google.gdata.data.MediaContent();
+                mc.setUri(resizeRequest.photoUrl);
+                final MediaSource media = myService.getMedia(mc);
+                final InputStream input = media.getInputStream();
+                try {
+                    final BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(resizeRequest.originalFile));
+                    try {
+                        IOUtils.copy(input, output);
+                    }
+                    finally {
+                        IOUtils.closeQuietly(output);
+                    }
+                }
+                finally {
+                    IOUtils.closeQuietly(input);
+                }
+                
+                System.out.println("\t\tDownloaded " + resizeRequest.photoEntry.getTitle().getPlainText());
+                return resizePool.submit(new ResizeWorker(resizeRequest));
             }
-            finally {
-                IOUtils.closeQuietly(output);
+            catch (Exception e) {
+                if (t < 10) {
+                    System.out.println("\t\tRetry Download " + resizeRequest.photoEntry.getTitle().getPlainText() + " - " + e.getMessage());
+                }
+                else {
+                    throw e;
+                }
             }
         }
-        finally {
-            IOUtils.closeQuietly(input);
-        }
-        
-        System.out.println("\t\tDownloaded " + resizeRequest.photoEntry.getTitle().getPlainText());
-        return resizePool.submit(new ResizeWorker(resizeRequest));
     }
     
-    private static void upload(ResizeRequest resizeRequest) throws IOException, ServiceException {
-        System.out.println("\t\tUploading " + resizeRequest.photoEntry.getTitle().getPlainText());
-        final MediaFileSource mediaFileSource = new MediaFileSource(resizeRequest.resizedFile, resizeRequest.type);
-        resizeRequest.photoEntry.setMediaSource(mediaFileSource);
-        resizeRequest.photoEntry.updateMedia(true);
-        
-        //Remove the resized file
-        resizeRequest.resizedFile.delete();
-        
-        System.out.println("\t\tUploaded " + resizeRequest.photoEntry.getTitle().getPlainText());
+    private static void upload(ResizeRequest resizeRequest) throws Exception {
+        for (int t = 0; ; t++) {
+            try {
+                System.out.println("\t\tUploading " + resizeRequest.photoEntry.getTitle().getPlainText());
+                final MediaFileSource mediaFileSource = new MediaFileSource(resizeRequest.resizedFile, resizeRequest.type);
+                resizeRequest.photoEntry.setMediaSource(mediaFileSource);
+                resizeRequest.photoEntry.updateMedia(true);
+                
+                //Remove the resized file
+                resizeRequest.resizedFile.delete();
+                
+                System.out.println("\t\tUploaded " + resizeRequest.photoEntry.getTitle().getPlainText());
+                
+                return;
+            }
+            catch (Exception e) {
+                if (t < 10) {
+                    System.out.println("\t\tRetry Upload " + resizeRequest.photoEntry.getTitle().getPlainText() + " - " + e.getMessage());
+                }
+                else {
+                    throw e;
+                }
+            }
+        }
     }
     
     private static final class ResizeRequest {
